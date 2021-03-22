c360fc9a9125820d603da6e83d79f2febc6a2f5d
husinhabsyi86 <husinhabsyi@gmail.com>
2021-03-22 16:05:21 +0700

    last

--- module-info.java
+++ module-info.java
@@ -1,31 +1,44 @@
-
-//TestNG Todo : Sample App
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import java.net.MalformedURLException;
-import java.net.URL;
+
+
+public class Example  {
+
+    public String baseUrl= "https://demo.midtrans.com";
+    public WebDriver driver = new FirefoxDriver();
 
   public class module-info(){
-	  
-     
+   
     public void testSimple() throws Exception 
-    
-    {
-      
-              //Change it to production page
+   
+    {            
               driver.get("https://demo.midtrans.com/");
              //Let's mark done first two items in the list.
               driver.findElement(By.name("Buy Now")).click();
               driver.findElement(By.name("CHECKOUT")).click();
               driver.findElement(By.name("CONTINUE")).click();
+              
+              
+              public void verifyHomePageTitle() {
+            	  driver.get(baseUrl);
+              WebElement element = driver.findElement(By.name("Name"));
+              element.sendKeys("Husin");
+              WebElement element = driver.findElement(By.name("Email"));
+              element.sendKeys("Husinhabsyi@gmail.com");
+              WebElement element = driver.findElement(By.name("PhoneNo"));
+              element.sendKeys("00000000");
+              
+              driver.findElement(By.name("CHECKOUT")).click();
+              
+              System.out.println("Page title is: " + driver.getTitle());
+              driver.quit();
+              
+              
+              
+              
              
     }
   }
